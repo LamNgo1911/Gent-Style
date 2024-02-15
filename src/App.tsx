42853c41a8fc0771ead8c43efc2cea21296cb8ca
Diddayngo <lamngo606@gmail.com>
2024-02-15 18:45:17 +0200

    installing react-router, create pages and components and, adding route for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ProductListing from "./pages/ProductListing";
+import ProductDetail from "./pages/ProductDetail";
+import ShoppingCart from "./pages/ShoppingCart";
+import Checkout from "./pages/Checkout";
+import OrderConfirmation from "./pages/OrderConfirmation";
+import AboutUs from "./pages/AboutUs";
+import SearchResults from "./pages/SearchResults";
+import ContactUs from "./pages/ContactUs";
+import Wishlist from "./pages/Wishlist";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/search-results" element={<SearchResults />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-confirmation" element={<OrderConfirmation />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
