48cb557e2cfd9bd41fc1a7708a9e35c0fd9797fd
Diddayngo <lamngo606@gmail.com>
2024-02-20 01:13:34 +0200

    simple login/register page completed

--- src/pages/auth/Login/Login.tsx
+++ src/pages/auth/Login/Login.tsx
@@ -1,6 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
+import { useForm } from "react-hook-form";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = (data: any) => {
+    // Validate credentials and handle login
+    navigate("/dashboard");
+  };
+
+  const clientId =
+    "775960901268-sqpvtimc7vdd13u2uafrrst6cpj8cj34.apps.googleusercontent.com";
+
+  const handleSuccess = (credentialResponse: any) => {
+    if (credentialResponse.credential) {
+      const credentialResponseDecode = jwtDecode(credentialResponse.credential);
+      console.log(credentialResponseDecode);
+    }
+  };
+
+  const handleFailure = (error: any) => {
+    console.log("Google login failure:", error);
+  };
+
+  return (
+    <div className="login-container">
+      <h2 className="login__title">Welcome back</h2>
+      <p className="login__subtitle">
+        Enter your email and password to access your account
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)} className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+          />
+          {errors.email && <span className="error">Email is required</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+          {errors.password && (
+            <span className="error">Password is required</span>
+          )}
+        </div>
+        <div className="form-check">
+          <div className="form-check__input">
+            <input
+              type="checkbox"
+              id="remember-me"
+              {...register("rememberMe")}
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <small className="forgot-password">Forgot password?</small>
+        </div>
+        <button type="submit" className="submit-btn">
+          Log in
+        </button>
+      </form>
+      <div className="google-login">
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin
+            text="signin_with"
+            onSuccess={handleSuccess}
+            onError={() => handleFailure}
+          />
+        </GoogleOAuthProvider>
+      </div>
+    </div>
+  );
 }
--- src/pages/auth/Register/Register.tsx
+++ src/pages/auth/Register/Register.tsx
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import "./Register.scss";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Register() {
   const {
     register,
     handleSubmit,
     formState: { errors },
     watch,
   } = useForm();
+
   const password = watch("password");
+  const navigate = useNavigate();
 
   const onSubmit = (data: any) => {
-    console.log(data);
+    navigate("/login");
   };
+  console.log(errors.email);
+
+  const clientId =
+    "775960901268-sqpvtimc7vdd13u2uafrrst6cpj8cj34.apps.googleusercontent.com";
+
+  const [googleLoginSuccess, setGoogleLoginSuccess] = useState(false);
+  const [googleLoginError, setGoogleLoginError] = useState(false);
+
+  const handleSuccess = (credentialResponse: any) => {
+    if (credentialResponse.credential) {
+      const credentialResponseDecode = jwtDecode(credentialResponse.credential);
+      
+      console.log(credentialResponseDecode);
+    }
+    setGoogleLoginSuccess(true);
+    setGoogleLoginError(false);
+ 
+  };
+
+  const handleFailure = (error: any) => {
+    console.log("Login failure:", error);
+    setGoogleLoginSuccess(false);
+    setGoogleLoginError(true);
+   
+  };
+
+  const emailInputRef = useRef<HTMLInputElement>(null);
+
+  // useEffect(() => {
+  //   emailInputRef.current?.focus();
+  // }, []);
+
+  console.log("Email value:", watch("email"));
+  console.log("password value:", watch("password"));
 
   return (
     <div className="register-container">
-      <h2 className="register__title">Register</h2>
+      <h2 className="register__title">Create your account</h2>
+      <small className="register__welcome">
+        Welcome to GentStyle. Lets create your account
+      </small>
       <form onSubmit={handleSubmit(onSubmit)} className="register-form">
         <div className="form-group">
           <label htmlFor="email">Email</label>
@@ -26,8 +68,13 @@ export default function Login() {
             type="email"
             id="email"
             placeholder="Enter your email"
+            // ref={emailInputRef}
           />
-          {errors.email && <span className="error">Email is required</span>}
+          {errors.email && (
+            <span className="error">
+              {errors.email.type === "required" ? "Email is required" : ""}
+            </span>
+          )}
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
@@ -57,27 +104,35 @@ export default function Login() {
           )}
         </div>
         <div className="form-terms">
-          <input
-            type="checkbox"
-            id="terms"
-            {...register("terms", { required: true })}
-          />
-          <label htmlFor="terms">I agree to the terms and conditions</label>
+          <div className="form-terms__input">
+            <input
+              type="checkbox"
+              id="terms"
+              {...register("terms", { required: true })}
+            />
+            <label htmlFor="terms">I agree to the terms and conditions</label>
+          </div>
           {errors.terms && (
             <span className="error">Please accept the terms</span>
           )}
         </div>
         <button type="submit" className="submit-btn">
-          Register
+          Create account
         </button>
       </form>
-
       <div className="register__small-text">
-        <span>-------------</span>
-        <small>& Register with</small>
-        <span>-------------</span>
+        <span className="register__small-decor"></span>
+        <small>& Create account with</small>
+        <span className="register__small-decor"></span>
       </div>
-      {/*fb and google register */}
+      {/* Google register */}
+      <GoogleOAuthProvider clientId={clientId}>
+        <GoogleLogin
+          text="signup_with"
+          onSuccess={() => handleSuccess}
+          onError={() => handleFailure}
+        />
+      </GoogleOAuthProvider>
     </div>
   );
 }
